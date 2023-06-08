e00db472b7014d77930e4d212a8f7e8a751a0e7a
Your Name <you@example.com>
2023-06-08 18:02:59 -0400

    entrega1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,88 @@
-
-import { Tabla } from "./componentes/tablas"
-import "./componentes/tablas.css"
-import "./App.css"
-
+import { faPlay, faForward, faBackward, faPause } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./componentes/tablas.css";
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  // Estado para almacenar la lista de canciones
+  const [canciones, setCanciones] = useState([]);
+  const [indiceCancion, setIndiceCancion] = useState(0); // Estado para controlar el índice de la canción actual
+  const audioRef = useRef(null);
+  const baseUrl = "https://assets.breatheco.de/apis/sound/";
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  // const inputRef= useRef(null);
-  // const textoInput= useRef("hola")
+  // Efecto para obtener las canciones al cargar el componente
+  useEffect(() => {
+    GetCanciones("https://assets.breatheco.de/apis/sound/songs");
+  }, []);
 
-  // const botonInput=()=>{
-  //   textoInput.current.innerHTML = inputRef.current.value;
-  //   <input ref={inputRef} type="text" />
-  //   <button  onClick={botonInput}>clic</button>
-  //   <div ref={textoInput}></div>
+  // Función para obtener las canciones mediante una solicitud HTTP
+  const GetCanciones = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCanciones(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  // }
- 
+  // Manejador de play y pause
+  const PlayPause = () => {
+    const audioElement = audioRef.current;
+
+    if (audioElement.paused) {
+      audioElement.play();
+      setIsPlaying(true);
+    } else {
+      audioElement.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  // Manejador para el botón de avanzar
+  const clicAvanzar = () => {
+    const siguienteCancion = (indiceCancion + 1) % canciones.length;
+    setIndiceCancion(siguienteCancion);
+    setIsPlaying(false);
+  };
+
+  // Manejador  botón de retroceder
+  const clicRetroceder = () => {
+    const cancionAnterior = (indiceCancion - 1 + canciones.length) % canciones.length;
+    setIndiceCancion(cancionAnterior);
+    setIsPlaying(false);
+  };
 
   return (
     <>
-    <Tabla />
+      <div className="titulo">Reproductor De Musica</div>
+      {canciones.map((cancion, index) => (
+        <div className="cancion" key={cancion.id}>
+          <h3>{cancion.name}</h3>
+          {index === indiceCancion && (
+            <audio ref={audioRef}  src={baseUrl + cancion.url} type="audio/mpeg" />
+          )}
+        </div>
+      ))}
+      <div className="fijo">
+        {canciones.length > 0 && (
+          <audio ref={audioRef}  src={baseUrl + canciones[indiceCancion]?.url} type="audio/mpeg" />
+        )}
+        <div className="retroceder" onClick={clicRetroceder}>
+          <FontAwesomeIcon icon={faBackward} />
+        </div>
+        <div className="play" onClick={PlayPause}>
+          <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+        </div>
+        <div className="adelantar" onClick={clicAvanzar}>
+          <FontAwesomeIcon icon={faForward} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/tablas.jsx
+++ src/componentes/tablas.jsx
@@ -1,32 +1,25 @@
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { faBackward } from "@fortawesome/free-solid-svg-icons";
-import { faForward } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlay,faForward,faBackward} from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Tabla =()=>{
-return(
-    <>
-    <div className="titulo">Reproductor De Musica</div>
-    
-    <div className="canciones">cancion1</div>
-    <div className="canciones">cancion2</div>
-    <div className="canciones">cancion3</div>
-    <div className="canciones">cancion4</div>
-    <div className="canciones">cancion5</div>
-    <div className="canciones">cancion6</div>
-    <div className="canciones">cancion7</div>
-    <div className="canciones">cancion8</div>
-    <div className="canciones">cancion9</div>
-    <div className="canciones">cancion10</div>
-    <div className="canciones">cancion11</div>
-    <div className="canciones">cancion12</div>
-    <div className="fijo">
-        <div className="retroceder"><FontAwesomeIcon icon={faBackward} /></div>
-        <div className="play"><FontAwesomeIcon icon={faPlay} /> </div>
-        <div className="adelantar"><FontAwesomeIcon icon={faForward} /></div>
-    </div>
-    </>
-)
-};
 
-export {Tabla};
+// const ListaCanciones = ({ canciones }) => {
+//   const baseUrl = "https://assets.breatheco.de/apis/sound/";
+  
+
+//   return (
+//     <>
+//       <div className="titulo">Reproductor De Musica</div>
+//       {canciones.map((cancion) => (
+        
+//         <div className="cancion" key={cancion.id}>
+
+//           <h3>{cancion.name}</h3>
+
+//           <audio src={baseUrl+cancion.url} type="audio/mpeg" />
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export {ListaCanciones};
